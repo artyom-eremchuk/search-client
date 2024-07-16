7533d2ba6f97077b8f0a7a7253febf8016865908
artyom-eremchuk <artyom.eremchuk@gmail.com>
2024-07-16 12:40:03 +0300

    create init project

---
+++ backend/db.ts
@@ -0,0 +1,32 @@
+const db = [
+  {
+    email: "jim@gmail.com",
+    number: "221122",
+  },
+  {
+    email: "jam@gmail.com",
+    number: "830347",
+  },
+  {
+    email: "john@gmail.co",
+    number: "221122",
+  },
+  {
+    email: "jams@gmail.com",
+    number: "349425",
+  },
+  {
+    email: "jams@gmail.com",
+    number: "141424",
+  },
+  {
+    email: "jill@gmail.com",
+    number: "822287",
+  },
+  {
+    email: "jill@gmail.com",
+    number: "822286",
+  },
+];
+
+export default db;
